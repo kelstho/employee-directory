9b04f635fce4db6b62e8eacfff5b26bc8c845a16
Kelly Thompson <ksthompson48@gmail.com>
2020-05-05 14:31:08 -0500

    fix API

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASEURL = "https://randomuser.me/api/?results=25";
+const BASEURL = "https://randomuser.me/api?nat=us&results=25";
 
 export default {
   search: function(query) {
