21ce2e7e409c1a92afdd687ed57c958116b33904
Kelly Thompson <ksthompson48@gmail.com>
2020-05-05 14:33:08 -0500

    finalize table for population from API

---
+++ src/components/List.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function List(props) {
+  const field = (props.sortby === "firstNameSort") ? "first" : "last";
+  return (
+    <div className="table-responsive">
+      <table className="table">
+        <tbody>
+        {props.results.sort((a, b) => a.name[field] > b.name[field] ? 1 : -1).map(result => (
+          <tr key={result.id.value}>
+            <td><img className="img-fluid" alt={result.name.first} src={result.picture.large} /></td>
+            <td>{`${result.name.first} ${result.name.last}`}</td>
+            <td>{result.cell}</td>
+            <td>{result.email}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default List;
