78413d5aeae572c4a3345d65b937baf7051a0819
Kelly Thompson <ksthompson48@gmail.com>
2020-05-05 10:21:46 -0500

    update API

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASEURL = "https://randomuser.me/api/?results=25";
 
 export default {
-  generateUsers: function () {
-    return axios.get(BASEURL);
+  search: function(query) {
+    return axios.get(BASEURL + query);
   }
 };
